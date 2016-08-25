04b963ccf75a0c9582e23124209a43448de45d18
Sharon Smith <Sharon Smith>
2016-08-24 22:36:37 -0400

    homework step 1

---
+++ mined_minds_function2.rb
@@ -0,0 +1,5 @@
+require "minitest/autorun"
+
+def mined_minds(number)
+	1 
+end
---
+++ test_mined_minds_function2.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require_relative "mined_minds_function2.rb"
+
+class TestMinedMindsFunction2 < Minitest::Test
+	def test_1_equals_1
+		assert_equal(1, 1) 
+	end
+
+end
